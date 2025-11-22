989db246b4e916baa8aea3dd7af1feff02838c0a
pitercoding <piterg.bio@gmail.com>
2025-11-22 23:21:23 +0100

    chore(config): configurar ambiente com Next.js, TailwindCSS e integração inicial do Firebase

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return(
+    <div className="flex h-screen justify-center items-center bg-gradient-to-r from-purple-500 via-yellow-600 to-blue-600">
+      <span className="text-4xl">Tailwind Funionando!</span>
+    </div>
+  ) 
+}
