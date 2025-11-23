c4c43821847bd5289a3bacb530e18d5b46ad4cf3
pitercoding <piterg.bio@gmail.com>
2025-11-23 15:49:31 +0100

    feat(tabela): adicionar componente de tabela com ações, estilos e ícones de edição e exclusão
    incluindo:
    - renderização de linhas e cabeçalho
    - ícones de edição e exclusão
    - botão de ação com eventos
    - integração com o layout da Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,35 @@
-import Layout from "@/components/Layout"
+"use client";
+
+import Layout from "@/components/Layout";
+import Tabela from "@/components/Tabela";
+import Cliente from "@/core/Cliente";
 
 export default function Home() {
-  return(
-    <div className={`
-    flex h-screen justify-center items-center bg-linear-to-r from-blue-600 to-purple-500 text-white
-    `}>
+  const clientes = [
+    new Cliente("Ana", 34, "1"),
+    new Cliente("Bia", 45, "2"),
+    new Cliente("Carlos", 23, "3"),
+    new Cliente("Pedro", 54, "4"),
+  ];
+
+  function clienteSelecionado(cliente: Cliente) {
+    console.log(cliente.nome);
+  }
+
+  function clienteExcluido(cliente: Cliente) {
+    console.log(`Excluir... ${cliente.nome}`);
+  }
+
+  return (
+    <div
+      className="
+        flex h-screen justify-center items-center 
+        bg-linear-to-r from-blue-600 to-purple-500 text-white
+      "
+    >
       <Layout titulo="Cadastro Simples">
-        <span>Conteúdo</span>
+        <Tabela clientes={clientes} clienteSelecionado={clienteSelecionado} clienteExcluido={clienteExcluido} />
       </Layout>
     </div>
-  ) 
+  );
 }
