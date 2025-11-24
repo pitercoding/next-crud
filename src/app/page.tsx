6d6a9f4954c5f989581f393b9e0afda7d864ed3c
pitercoding <piterg.bio@gmail.com>
2025-11-24 13:09:51 +0100

    feat: integrar Tabela e Formulário com fluxo completo de seleção, edição e criação de cliente
    - Ao clicar em "Alterar" na tabela, agora retorna para a tabela automaticamente após salvar
    - Criado estado global para armazenar o cliente atual (setCliente)
    - Seleção de cliente na tabela atualiza corretamente o formulário
    - Botão "Novo Cliente" volta a exibir um cliente vazio, evitando reaproveitar o último cliente selecionado
    - Função novoCliente() agora limpa o cliente e exibe o formulário no modo criação

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,10 @@ import Formulario from "@/components/Formulario";
 import { useState } from "react";
 
 export default function Home() {
+
+  const [cliente, setCliente] = useState<Cliente>(Cliente.vazio())
+  const [visivel, setVisivel] = useState<"tabela" | "form">("tabela");
+
   const clientes = [
     new Cliente("Ana", 34, "1"),
     new Cliente("Bia", 45, "2"),
@@ -16,18 +20,23 @@ export default function Home() {
   ];
 
   function clienteSelecionado(cliente: Cliente) {
-    console.log(cliente.nome);
+    setCliente(cliente)
+    setVisivel('form')
   }
 
   function clienteExcluido(cliente: Cliente) {
     console.log(`Excluir... ${cliente.nome}`);
   }
 
-  function salvarCliente(cliente: Cliente) {
-    console.log(cliente)
+  function novoCliente() {
+    setCliente(Cliente.vazio())
+    setVisivel('form')
   }
 
-  const [visivel, setVisivel] = useState<"tabela" | "form">("tabela");
+  function salvarCliente(cliente: Cliente) {
+    console.log(cliente)
+    setVisivel('tabela')
+  }
 
   return (
     <div
@@ -41,7 +50,7 @@ export default function Home() {
           <>
             <div className="flex justify-end">
               <Botao cor="green" className="mb-4"
-              onClick={() => setVisivel('form')}>
+              onClick={novoCliente}>
                 Novo Cliente
               </Botao>
             </div>
@@ -53,7 +62,7 @@ export default function Home() {
           </>
         ) : (
           <Formulario
-            cliente={clientes[2]}
+            cliente={cliente}
             clienteMudou={salvarCliente}
             cancelado={() => setVisivel('tabela')}
           />
